107143995dc6c7574088e2cddaadf80afc21c854
Sylvie586 <jogjegyzet@gmail.com>
2017-01-24 12:07:53 +0100

    Adding node files, index.html and index.js

---
+++ node.stuff.js
@@ -0,0 +1,81 @@
+<<<<<<< HEAD
+//  -------------------------------------------------------------------------------------------------------------
+//  auxilary routines
+//  -------------------------------------------------------------------------------------------------------------
+
+module.exports = {
+
+//  --- start of of module.exports ---
+
+//  routine to convert timestamps to log format
+
+  timestamp2log: function(timestamp) {
+    var ts = timestamp.getFullYear()+'/';
+    
+    var temp = timestamp.getMonth()+1;
+    if (Number(temp) < 10) temp = '0'+temp;
+    ts += temp + '/';
+
+    var temp = timestamp.getDate();
+    if (Number(temp) < 10) temp = '0'+temp;
+    ts += temp + ' ';
+    
+    var temp = timestamp.getHours();
+    if (Number(temp) < 10) temp = '0'+temp;
+    ts += temp + ':';
+    
+    var temp = timestamp.getMinutes();
+    if (Number(temp) < 10) temp = '0'+temp;
+    ts += temp + ':';
+    
+    var temp = timestamp.getSeconds();
+    if (Number(temp) < 10) temp = '0'+temp;
+    ts += temp + ' ';
+    
+    return ts;
+  }
+
+//  --- end of module.exports ---
+
+}
+=======
+//  -------------------------------------------------------------------------------------------------------------
+//  auxilary routines
+//  -------------------------------------------------------------------------------------------------------------
+
+module.exports = {
+
+//  --- start of of module.exports ---
+
+//  routine to convert timestamps to log format
+
+  timestamp2log: function(timestamp) {
+    var ts = timestamp.getFullYear()+'/';
+    
+    var temp = timestamp.getMonth()+1;
+    if (Number(temp) < 10) temp = '0'+temp;
+    ts += temp + '/';
+
+    var temp = timestamp.getDate();
+    if (Number(temp) < 10) temp = '0'+temp;
+    ts += temp + ' ';
+    
+    var temp = timestamp.getHours();
+    if (Number(temp) < 10) temp = '0'+temp;
+    ts += temp + ':';
+    
+    var temp = timestamp.getMinutes();
+    if (Number(temp) < 10) temp = '0'+temp;
+    ts += temp + ':';
+    
+    var temp = timestamp.getSeconds();
+    if (Number(temp) < 10) temp = '0'+temp;
+    ts += temp + ' ';
+    
+    return ts;
+  }
+
+//  --- end of module.exports ---
+
+}
+>>>>>>> c563de8311cf2d808b64aa74ae40acdd41c30ccd
